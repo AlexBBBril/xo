e3d00978ec7924460dcdb8133ca3d0fade6239a1
bril <aleksey.bril@renlife.com>
2020-04-16 10:18:10 +0300

    field realisation

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,71 @@
+import io.hexlet.xo.model.*;
+
+import java.util.Random;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println(123123);
+    private static final io.hexlet.xo.model.Field field = new io.hexlet.xo.model.Field();
+    private static final Random r = new Random();
+    private static final String[] figures = {"X", "O"};
+    private static final String[] figuresOnField = new String[field.getSize() * field.getSize()];
+    private static int allFiguresCounter = 0;
+
+    public static void main(String... args) {
+
+        testSetFigure();
+        testGetFigure();
+
+    }
+
+    private static void testSetFigure() {
+
+        for (int i = 0; i < field.getSize(); i++) {
+            for (int i2 = 0; i2 < field.getSize(); i2++) {
+                final Point p = new Point();
+                p.x = i;
+                p.y = i2;
+
+                String randomFigure;
+                figuresOnField[allFiguresCounter++] = randomFigure = figures[r.nextInt(2)];
+                field.setFigure(p, randomFigure);
+
+                java.lang.reflect.Field f = null;
+                try {
+                    f = io.hexlet.xo.model.Field.class.getDeclaredField(String.format("f%d%d", i, i2));
+                    f.setAccessible(true);
+                } catch (NoSuchFieldException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    String realFigureOnField = (String) f.get(field);
+                    if (!randomFigure.equals(realFigureOnField)) {
+                        throw new RuntimeException(String.format(
+                                "\nYOUR method \"setFigure\" did not set the figure \"%s\" in the coordinates: x = %d, y = %d to the variable \"f%d%d\".",
+                                randomFigure, i, i2, i, i2));
+                    }
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
+
+    private static void testGetFigure() {
+        allFiguresCounter = 0;
+        for (int i = 0; i < field.getSize(); i++) {
+            for (int i2 = 0; i2 < field.getSize(); i2++) {
+                final Point p = new Point();
+                p.x = i;
+                p.y = i2;
+                final String randomFigure = figuresOnField[allFiguresCounter++];
+
+                String actualFigure = field.getFigure(p);
+
+                if (!randomFigure.equals(actualFigure)) {
+                    throw new RuntimeException(String.format("\nFigure on coordinates: x = %d, y = %d equals to \"%s\" instead of \"%s\".", i, i2, actualFigure, randomFigure));
+                }
+            }
+        }
     }
 }
--- src/io/hexlet/xo/model/Field.java
+++ src/io/hexlet/xo/model/Field.java
@@ -24,11 +24,47 @@ public class Field {
         return 3;
     }
 
-    public String getFigure(final Point point) {
-        return null;
+    public String getFigure(final Point p) {
+        if (p.x == 0 && p.y == 0) return f00;
+        if (p.x == 1 && p.y == 0) return f10;
+        if (p.x == 2 && p.y == 0) return f20;
+        if (p.x == 0 && p.y == 1) return f01;
+        if (p.x == 1 && p.y == 1) return f11;
+        if (p.x == 2 && p.y == 1) return f21;
+        if (p.x == 0 && p.y == 2) return f02;
+        if (p.x == 1 && p.y == 2) return f12;
+        if (p.x == 2 && p.y == 2) return f22;
+
+        throw new RuntimeException("OLOLOLOL");
     }
 
-    public void setFigure(final Point point, final String figure) {
-
+    public void setFigure(final Point p, final String figure) {
+        if (p.x == 0 && p.y == 0){
+            f00 = figure;
+        }
+        if (p.x == 1 && p.y == 0){
+            f10 = figure;
+        }
+        if (p.x == 2 && p.y == 0){
+            f20 = figure;
+        }
+        if (p.x == 0 && p.y == 1){
+            f01 = figure;
+        }
+        if (p.x == 1 && p.y == 1){
+            f11 = figure;
+        }
+        if (p.x == 2 && p.y == 1){
+            f21 = figure;
+        }
+        if (p.x == 0 && p.y == 2){
+            f02 = figure;
+        }
+        if (p.x == 1 && p.y == 2){
+            f12 = figure;
+        }
+        if (p.x == 2 && p.y == 2){
+            f22 = figure;
+        }
     }
 }
