22231d47119bf54c472b1a945a117a47370fb5a6
bril <aleksey.bril@renlife.com>
2020-04-26 23:24:43 +0300

    AICoordinateGetter Tic Tac Toe AI

--- src/Main.java
+++ src/Main.java
@@ -1,79 +1,37 @@
 import io.hexlet.xo.model.*;
 import io.hexlet.xo.model.Point;
 import io.hexlet.xo.view.ConsoleView;
+import io.hexlet.xo.view.ICoordinateGetter;
+import io.hexlet.xo.view.RandomCoordinateGetter;
+
+import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String... args) {
-        final Player p1 = new Player("Slava", Figure.X);
-        final Player p2 = new Player("Gleb", Figure.O);
         final Field field = new Field();
-        final Game game = new GameBuilder().player1(p1).player2(p2).field(field).name("XO").build();
-        final ConsoleView cv = new ConsoleView();
+        final ICoordinateGetter view = new ConsoleView();
+        final ICoordinateGetter random = new RandomCoordinateGetter();
+        IntStream.range(0, 9).forEach(
+                i -> {
+                    final Point p = random.getMoveCoordinate(field);
+                    field.setFigure(p, Figure.X);
+                }
+        );
+        final int sum = (int)IntStream.range(0, 3).mapToLong(
+                i -> {
+                    return (int) IntStream.range(0, 3).mapToLong(
+                            i2 -> {
+                                if (field.getFigure(new Point(i, i2)) != null) return (Integer)1;
+                                return (Integer)0;
+                            }
+                    ).sum();
+                }
+        ).sum();
 
-
-        field.setFigure(new Point(0, 0), Figure.O);
-        field.setFigure(new Point(1, 0), Figure.X);
-        field.setFigure(new Point(0, 1), Figure.X);
-        field.setFigure(new Point(2, 2), Figure.O);
-
-
-        // test line
-        final StringBuilder lineBuilder = new StringBuilder();
-        final String expectedLine = " O | X |   \n";
-        cv.generateLine(field, 0, lineBuilder);
-        final String actualLine = lineBuilder.toString();
-
-        if (!expectedLine.equals(actualLine)) {
-            throw new RuntimeException(String.format("Line generated:\n01234567890\n%s instead of:\n01234567890\n%s", actualLine, expectedLine));
-        }
-
-        // test separator
-
-
-        // test full Field
-        final String expectedField = " O | X |   \n" +
-                "~~~~~~~~~~~\n" +
-                " X |   |   \n" +
-                "~~~~~~~~~~~\n" +
-                "   |   | O \n";
-
-        cv.show(game);
-        final String actualField = cv.getFieldBuilder().toString();
-
-        if (!expectedField.equals(actualField)) {
-            throw new RuntimeException(String.format("Field generated:\n01234567890\n%s instead of:\n01234567890\n%s", actualField, expectedField));
-        }
-
-        otherTest();
+        ((ConsoleView)view).show(field);
+        if (sum != 9)
+            throw new RuntimeException(String.format("field incorrectly. After 9 iterations it should be fully occupied"));
     }
 
-    private static void otherTest() {
-        final Player p1 = new Player("Slava", Figure.X);
-        final Player p2 = new Player("Gleb", Figure.O);
-        final Field field = new Field();
-        final Game game = new GameBuilder().player1(p1).player2(p2).field(field).name("XO").build();
-        final ConsoleView cv = new ConsoleView();
-
-
-        field.setFigure(new Point(2, 0), Figure.X);
-        field.setFigure(new Point(0, 1), Figure.O);
-        field.setFigure(new Point(1, 0), Figure.X);
-        field.setFigure(new Point(1, 1), Figure.O);
-        field.setFigure(new Point(0, 0), Figure.X);
-
-        // test full Field
-        final String expectedField = " X | O |   \n" +
-                "~~~~~~~~~~~\n" +
-                " X | O |   \n" +
-                "~~~~~~~~~~~\n" +
-                " X |   |   \n";
-
-        cv.show(game);
-        final String actualField = cv.getFieldBuilder().toString();
-
-        if (!expectedField.equals(actualField)) {
-            throw new RuntimeException(String.format("Field generated:\n01234567890\n%s instead of:\n01234567890\n%s", actualField, expectedField));
-        }
-    }
 }
---
+++ src/io/hexlet/xo/view/AICoordinateGetter.java
@@ -0,0 +1,13 @@
+package io.hexlet.xo.view;
+
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Point;
+
+public class AICoordinateGetter implements ICoordinateGetter {
+
+    @Override
+    public Point getMoveCoordinate(Field field) {
+        return null;
+    }
+
+}
