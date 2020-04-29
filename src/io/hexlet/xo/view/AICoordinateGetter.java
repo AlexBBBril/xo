fb197e7cab1d9e80363f9a44bb913e98b6c77601
bril <aleksey.bril@renlife.com>
2020-04-29 09:42:10 +0300

    AICoordinateGetter Tic Tac Toe AI

--- src/io/hexlet/xo/view/AICoordinateGetter.java
+++ src/io/hexlet/xo/view/AICoordinateGetter.java
@@ -1,12 +1,31 @@
 package io.hexlet.xo.view;
 
 import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Figure;
 import io.hexlet.xo.model.Point;
 
 public class AICoordinateGetter implements ICoordinateGetter {
+    private final Point[] STEPS = {
+            new Point(1,1),
+            new Point(0,0),
+            new Point(0,2),
+            new Point(2,0),
+            new Point(2,2),
+            new Point(0,1),
+            new Point(1,0),
+            new Point(1,2),
+            new Point(2,1),
+    };
 
     @Override
     public Point getMoveCoordinate(Field field) {
+        for (Point point: STEPS) {
+            Figure figure = field.getFigure(point);
+            if (null == figure) {
+                return point;
+            }
+        }
+
         return null;
     }
 
