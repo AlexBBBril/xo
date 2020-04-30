01327dc6629671b293756017b1364804c0ae120b
bril <aleksey.bril@renlife.com>
2020-04-30 13:15:11 +0300

    Trows invalid coordinate exception

--- src/io/hexlet/xo/controllers/MoveController.java
+++ src/io/hexlet/xo/controllers/MoveController.java
@@ -3,25 +3,19 @@ package io.hexlet.xo.controllers;
 import io.hexlet.xo.model.Field;
 import io.hexlet.xo.model.Figure;
 import io.hexlet.xo.model.Point;
+import io.hexlet.xo.model.exceptions.AlreadyOccupiedException;
+import io.hexlet.xo.model.exceptions.InvalidPointException;
 
 public class MoveController {
 
-    public boolean applyFigure(final Field field,
+    public static void applyFigure(final Field field,
                                final Point point,
-                               final Figure figure) {
-        if (checkCoordinate(point.getX()) && checkCoordinate(point.getY()) && null == field.getFigure(point)) {
-
-            field.setFigure(point, figure);
-
-            return true;
+                               final Figure figure) throws InvalidPointException, AlreadyOccupiedException {
+        if (null != field.getFigure(point)) {
+            throw new AlreadyOccupiedException();
         }
 
-        return false;
-    }
-
-    private boolean checkCoordinate(final int coordinate) {
-
-        return coordinate >= 0 && coordinate < 3;
+        field.setFigure(point, figure);
     }
 
 }
--- src/io/hexlet/xo/model/Field.java
+++ src/io/hexlet/xo/model/Field.java
@@ -1,19 +1,43 @@
 package io.hexlet.xo.model;
 
+import io.hexlet.xo.model.exceptions.InvalidPointException;
+
 public class Field {
 
-    private final Figure[][] figures = new Figure[3][3];
+    private static final int MIN_COORDINATE = 0;
+
+    private final Figure[][] field;
+
+    private final int fieldSize;
+
+    public Field(final int fieldSize) {
+        this.fieldSize = fieldSize;
+        field = new Figure[fieldSize][fieldSize];
+    }
 
     public int getSize() {
-        return this.figures.length;
+        return fieldSize;
     }
 
-    public Figure getFigure(final Point point) {
-        return this.figures[point.getX()][point.getY()];
+    public Figure getFigure(final Point point) throws InvalidPointException {
+        checkCoordinate(point);
+
+        return this.field[point.getX()][point.getY()];
     }
 
-    public void setFigure(final Point point, final Figure figure) {
-        this.figures[point.getX()][point.getY()] = figure;
+    public void setFigure(final Point point, final Figure figure) throws InvalidPointException {
+        checkCoordinate(point);
+
+        this.field[point.getX()][point.getY()] = figure;
+    }
+
+    private void checkCoordinate(final Point point) throws InvalidPointException {
+        if (point.getY() < MIN_COORDINATE || point.getX() < MIN_COORDINATE) {
+            throw new InvalidPointException();
+        }
+        if (point.getY() > this.fieldSize - 1 || point.getX() > this.fieldSize - 1) {
+            throw new InvalidPointException();
+        }
     }
 
 }
